753dfd72d01876e4fa0e03fba06b422f90120995
Manasi V Kathale <mansikathale5@gmail.com>
2025-07-23 18:37:12 +0530

    Dark Mode & Font Added

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hello
+      <Button varient="primary">Click me</Button>
+      <Button varient="glass">Button</Button>
+    </div>
+  );
+}
