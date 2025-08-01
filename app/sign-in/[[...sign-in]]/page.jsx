1f50d0a4464de400d9d982d31188a3ddb8213116
Manasi V Kathale <mansikathale5@gmail.com>
2025-08-01 09:29:10 +0530

    Dashboard page added with inputed unique id

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,11 +1,12 @@
 "use client";
-import { SignIn } from "@clerk/nextjs";
+import { RedirectToSignIn, SignIn } from "@clerk/nextjs";
 import React from "react";
 
 const SignInPage = () => {
   return (
     <div className="flex justify-center pt-50">
       <SignIn />
+      {/* <RedirectToSignIn /> */}
     </div>
   );
 };
