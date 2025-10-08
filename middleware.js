5110f2f6f896b8c8dbfb70b2a959e229bfbce9a8
Manasi V Kathale <mansikathale5@gmail.com>
2025-10-08 17:51:33 +0530

    Comments Added for middleware js

--- middleware.js
+++ middleware.js
@@ -5,7 +5,10 @@ import {
 } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoutes = createRouteMatcher(["/dashboard(.*)", "/editor(.*)"]);
+const isProtectedRoutes = createRouteMatcher([
+  // "/dashboard(.*)",
+  // "/editor(.*)"
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
