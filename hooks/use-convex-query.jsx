d4eeb736ca108efed7ca0e0f024ddcf99571fdc9
Manasi V Kathale <mansikathale5@gmail.com>
2025-12-02 18:11:12 +0530

    Dashboard Successfully working

---
+++ hooks/use-convex-query.jsx
@@ -0,0 +1,37 @@
+import { useMutation } from "convex/react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export const useConvexQuery = (query, ...args) => {
+  const result = useQuery(query, ...args);
+
+  const [data, setData] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (result === undefined) {
+      setIsLoading(true);
+    } else {
+      try {
+        setData(result);
+        setError(null);
+      } catch (err) {
+        setError(err);
+        toast.error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }, [result]);
+
+  return { data, isLoading, error };
+};
+
+export const useConvexMutation = (mutation) => {
+  const result = useMutation(mutation);
+
+  const [data, setData] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+};
